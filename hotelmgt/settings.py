a4ffd659439375f79cea795e0b21176ed9ce037f
an3-bit <andrewmogambi12@gmail.com>
2025-03-11 23:25:25 +0300

    settings

--- hotelmgt/settings.py
+++ hotelmgt/settings.py
@@ -118,18 +118,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR]
+#STATIC_DIR = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [STATIC_DIR]
 
 if not DEBUG:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# Add this line
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# # Optional: If you use a separate static folder for development
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Optional: If you use a separate static folder for development
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
