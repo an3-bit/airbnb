9a2872b22f747c8d6797adc62d67d0e8eaa37061
an3-bit <andrewmogambi12@gmail.com>
2025-03-11 21:41:05 +0300

    static files

--- hotelmgt/settings.py
+++ hotelmgt/settings.py
@@ -120,7 +120,13 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR]
+# Add this line
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# # Optional: If you use a separate static folder for development
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
